2a3762b1181f3e4cd28c06644ca0440c6770ebb9
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-01-13 19:01:35 +0100

    Story page

--- app/story.js
+++ app/story.js
@@ -29,13 +29,86 @@ hero.addEventListener('mousemove', shadow);
 
 const DOM = {
   body: document.querySelector('body'),
-  cta: document.querySelector('.circ-btn')
+  cta: document.querySelector('.circ-btn'),
+  slides: document.querySelectorAll('.slide')
 }
 
 DOM.cta.addEventListener('click', showStory, false);
 
 function showStory() {
   DOM.body.classList.add('show-story');
+}
 
-  console.log('a')
+
+
+
+
+let activeSlideCount = 0;
+
+// Detect scroll direction
+function detectMouseWheelDirection(e) {
+  var delta = null,
+    direction = false;
+  if (!e) {
+    // if the event is not provided, we get it from the window object
+    e = window.event;
+  }
+  if (e.wheelDelta) {
+    // will work in most cases
+    delta = e.wheelDelta / 60;
+  } else if (e.detail) {
+    // fallback for Firefox
+    delta = -e.detail / 2;
+  }
+  if (delta !== null) {
+    direction = delta > 0 ? 'up' : 'down';
+  }
+
+  return direction;
+}
+
+var scrollCounter = 0;
+
+function handleMouseWheelDirection(direction) {
+  if (scrollCounter % 50 == 0) {
+    if (direction == 'down') {
+      // showSlider();
+      // showNextSlide();
+      showNextSlide();
+
+    } else if (direction == 'up') {
+      // showPrevSlide();
+      console.log('b');
+    } else {
+      // this means the direction of the mouse wheel could not be determined
+    }
+  }
+
+  scrollCounter++;
+}
+
+document.onmousewheel = function (e) {
+  handleMouseWheelDirection(detectMouseWheelDirection(e));
+};
+if (window.addEventListener) {
+  document.addEventListener('DOMMouseScroll', function (e) {
+    handleMouseWheelDirection(detectMouseWheelDirection(e));
+  });
+}
+
+
+
+function showNextSlide() {
+  activeSlideCount++;
+  if (DOM.slides.length == activeSlideCount) activeSlideCount = 0;
+  DOM.slides.forEach(i => i.classList.remove('slide-visible'))
+  DOM.slides[activeSlideCount].classList.add('slide-visible')
+}
+
+
+window.addEventListener('keydown', checkKey, false);
+
+function checkKey(e) {
+  // if (e.keyCode === 38) showPrevSlide();
+  if (e.keyCode === 40) showNextSlide();
 }
